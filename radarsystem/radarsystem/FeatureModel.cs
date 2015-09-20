f854e19cb20389f594abd3d851e7cf8834ef7ba3
lemondy <lemondy9@gmail.com>
2015-09-20 15:38:59 +0800

    修改结构
    将特征分析从form代码中提取出来，重新定义FeatureModel类和PointD 类

---
+++ radarsystem/radarsystem/FeatureModel.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace radarsystem
+{
+    public class FeatureModel
+    {
+        /**
+         * 特性分析处理类，这里面添加特性处理的代码
+         * param: list 是运行轨迹的一些点； count 是特性的个数
+         * */
+        public Dictionary<String, double> getTimeAndSpaceFeature(List<PointD> list,int count)
+        {
+            Dictionary<String, double> featDic = new Dictionary<String, double>();
+            //计算时域空域特征分析
+            double[] features = new double[count];
+
+            PointD[] p1 = new PointD[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                p1[i] = list[i];
+            }
+
+
+            double[] pX = new double[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                pX[i] = p1[i].X;
+            }
+            //算术平均值,arithmetic mean value
+            for (int i = 0; i < list.Count; i++)
+            {
+                features[0] += p1[i].X;
+            }
+            features[0] /= list.Count;
+            featDic.Add("算术平均值", features[0]);
+
+            //几何平均值,geometric mean
+            features[1] = 1.0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (p1[i].X != 0)
+                    features[1] *= p1[i].X;
+
+            }
+            features[1] = Math.Pow(features[1], 1 / list.Count);
+            featDic.Add("几何平均值", features[1]);
+
+
+            //均方根值,root mean square value
+            for (int i = 0; i < list.Count; i++)
+            {
+                features[2] += Math.Pow(p1[i].X, 2);
+            }
+            features[2] /= list.Count;
+            features[2] = Math.Pow(features[2], 1 / 2);
+            featDic.Add("均方根值", features[2]);
+
+            //方差, variance
+            for (int i = 0; i < list.Count; i++)
+            {
+                features[3] += Math.Pow(p1[i].X - features[0], 2);
+            }
+            features[3] /= list.Count - 1;
+            featDic.Add("方差", features[3]);
+
+            //标准差,standard deviation
+            for (int i = 0; i < list.Count; i++)
+            {
+                features[4] += Math.Pow(p1[i].X - features[0], 2);
+            }
+            features[4] /= list.Count;
+            features[4] = Math.Pow(features[4], 1 / 2);
+            featDic.Add("标准差", features[4]);
+
+            //波形指标,waveform indicators
+            features[5] = featDic["均方根值"] / Math.Abs(featDic["算术平均值"]);
+            featDic.Add("波形指标", features[5]);
+
+            //峰值指标,peak index
+            features[6] = pX.Max() / featDic["均方根值"];
+            featDic["峰值指标"] = features[6];
+
+            //脉冲指标,pulse factor
+            features[7] = pX.Max() / Math.Abs(featDic["算术平均值"]);
+            featDic["脉冲指标"] = features[7];
+
+            //方根幅值,root amplitude
+            for (int i = 0; i < list.Count; i++)
+            {
+                features[8] += Math.Pow(Math.Abs(p1[i].X), 1 / 2);
+            }
+            features[8] = Math.Pow(features[8] / list.Count, 2);
+            featDic["方根幅值"] = features[8];
+
+            //裕度指标,margin indicator
+            features[9] = pX.Max() / featDic["方根幅值"];
+            featDic["裕度指标"] = features[9];
+
+            //峭度指标,Kurosis amplitude
+            for (int i = 0; i < list.Count; i++)
+            {
+                features[10] += Math.Pow(p1[i].X, 4);
+            }
+            features[10] /= list.Count;
+            features[10] = features[10] / Math.Pow(featDic["均方根值"], 4);
+            featDic["峭度指标"] = features[10];
+
+            //自相关函数,m=2,autocorrelation
+            for (int i = 0; i < (list.Count - 2); i++)
+            {
+                features[11] += p1[i].X * p1[i + 2].X;
+            }
+            featDic["自相关函数"] = features[11];
+
+            //互相关函数,cross-correlation
+            for (int i = 0; i < (list.Count - 2); i++)
+            {
+                features[12] += p1[i].X * p1[i + 2].Y;
+            }
+            featDic["互相关函数"] = features[12];
+
+            return featDic;
+        }
+
+
+        /**
+         * 计算频域特性
+         **/
+        public Dictionary<String, Double> getFrequentFeature(List<PointD> list)
+        {
+            Dictionary<String, Double> frequencyFeature = new Dictionary<string, double>();
+
+            return frequencyFeature;
+        }
+       
+    }
+}
--- radarsystem/radarsystem/Form1.cs
+++ radarsystem/radarsystem/Form1.cs
@@ -16,8 +16,7 @@ namespace radarsystem
 {
     public partial class Form1 : Form
     {
-        //自适应窗口类
-     //   AutoSizeFormClass autoForm = new AutoSizeFormClass();
+      
 
         List<PointD> list = new List<PointD>();
         List<Point> list_trace = new List<Point>();
@@ -29,12 +28,8 @@ namespace radarsystem
         Thread t2;
         Thread t1;
      //用pictureBox4 的左上角坐标表示雷达的中心点坐标
-     //   ArrayList AL = new ArrayList();
-        public class PointD
-        {
-            public double X;
-            public double Y;
-        }
+   
+      
         public Form1()
         {
             InitializeComponent();
@@ -51,8 +46,7 @@ namespace radarsystem
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //autoForm.controllInitializeSize(this);
-            //MessageBox.Show("ha");
+            //连接数据库
             string ConStr = string.Format(@"Provider=Microsoft.Jet.OLEDB.4.0;
                             Data source="+Application.StartupPath+"\\database\\whut\\RecognitionAid.mdb");
             //MessageBox.Show(ConStr);
@@ -342,128 +336,17 @@ namespace radarsystem
             else MessageBox.Show("您选择添加了" + strCollected, "提示");
         }
 
-        private Dictionary<String,double> getTimeAndSpaceFeature(int count)
-        {
-            Dictionary<String,double> featDic = new Dictionary<String,double>();
-            //计算时域空域特征分析
-            double[] features = new double[count];
-
-            PointD[] p1 = new PointD[list.Count];
-            for (int i=0;i<list.Count;i++)
-            {
-                p1[i] = list[i];
-            }
-
-
-            double[] pX = new double[list.Count];
-            for (int i = 0; i < list.Count; i++)
-            {
-                pX[i] = p1[i].X;
-            }
-            //算术平均值,arithmetic mean value
-            for (int i = 0; i < list.Count; i++)
-            {
-                features[0] += p1[i].X;
-            }
-            features[0] /= list.Count;
-            featDic.Add("算术平均值", features[0]);
-
-            //几何平均值,geometric mean
-            features[1] = 1.0;
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (p1[i].X != 0)
-                    features[1] *= p1[i].X;
-
-            }
-            features[1] = Math.Pow(features[1], 1 / list.Count);
-            featDic.Add("几何平均值", features[1]);
-
-
-            //均方根值,root mean square value
-            for (int i = 0; i < list.Count; i++)
-            {
-                features[2] += Math.Pow(p1[i].X, 2);
-            }
-            features[2] /= list.Count;
-            features[2] = Math.Pow(features[2], 1 / 2);
-            featDic.Add("均方根值", features[2]);
-
-                //方差, variance
-            for (int i = 0; i < list.Count; i++)
-            {
-                features[3] += Math.Pow(p1[i].X - features[0],2);
-            }
-            features[3] /= list.Count-1;
-            featDic.Add("方差", features[3]);
-
-                //标准差,standard deviation
-            for (int i = 0; i < list.Count; i++)
-            {
-                features[4] += Math.Pow(p1[i].X - features[0], 2);
-            }
-            features[4] /= list.Count;
-            features[4] = Math.Pow(features[4], 1 / 2);
-            featDic.Add("标准差", features[4]);
-
-                //波形指标,waveform indicators
-            features[5] = featDic["均方根值"] / Math.Abs(featDic["算术平均值"]);
-            featDic.Add("波形指标", features[5]);
-
-                //峰值指标,peak index
-            features[6] = pX.Max() / featDic["均方根值"];
-            featDic["峰值指标"] = features[6];
-
-                //脉冲指标,pulse factor
-            features[7] = pX.Max() / Math.Abs(featDic["算术平均值"]);
-            featDic["脉冲指标"] = features[7];
-
-                //方根幅值,root amplitude
-            for (int i = 0; i < list.Count; i++)
-            {
-                features[8] += Math.Pow(Math.Abs(p1[i].X), 1 / 2);
-            }
-            features[8] = Math.Pow(features[8] / list.Count, 2);
-            featDic["方根幅值"] = features[8];
-
-                //裕度指标,margin indicator
-            features[9] = pX.Max() / featDic["方根幅值"];
-            featDic["裕度指标"] = features[9];
-
-                //峭度指标,Kurosis amplitude
-            for (int i = 0; i < list.Count; i++)
-            {
-                features[10] += Math.Pow(p1[i].X, 4);
-            }
-            features[10] /= list.Count;
-            features[10] = features[10] / Math.Pow(featDic["均方根值"], 4);
-            featDic["峭度指标"] = features[10];
-
-                //自相关函数,m=2,autocorrelation
-            for (int i = 0; i < (list.Count-2); i++)
-            {
-                features[11] += p1[i].X * p1[i + 2].X;
-            }
-            featDic["自相关函数"] = features[11];
-
-                //互相关函数,cross-correlation
-            for (int i = 0; i < (list.Count-2); i++)
-            {
-                features[12] += p1[i].X * p1[i + 2].Y;
-            }
-            featDic["互相关函数"] = features[12];
-
-                return featDic;
-        }
+       
 
         private void featurecomboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             
             int showIndex = featurecomboBox1.SelectedIndex;
+            FeatureModel feature = new FeatureModel();
             if (showIndex == 0)
             {
                 //当前选中了时域和空域特征分析
-                Dictionary<String, double> featDic = getTimeAndSpaceFeature(13);
+                Dictionary<String, double> featDic = feature.getTimeAndSpaceFeature(list,13);
                 String[] featName = new String[13];
                 int i = 0;
                 foreach (String key in featDic.Keys)
@@ -509,6 +392,7 @@ namespace radarsystem
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
+            
             //创建画板
             Graphics g = e.Graphics;
             Pen pen = new Pen(Color.Black, 1/2);
@@ -618,8 +502,13 @@ namespace radarsystem
             //autoForm.controlAutoSize(this);
         }
 
+        
+        /**
+         *  显示特性分析中X坐标轴的刻度
+         **/
         private void Xpanel_Paint(object sender, PaintEventArgs e)
         {
+            
             Graphics g = Xpanel.CreateGraphics();
             double start = -1;
             int sLoc = 0;
@@ -630,8 +519,13 @@ namespace radarsystem
             }
         }
 
+        /**
+         * 显示特性分析中Y轴坐标刻度
+         **/
+
         private void Ypanel_Paint(object sender, PaintEventArgs e)
         {
+            
             Graphics g = Ypanel.CreateGraphics();
             double start = 1;
             int sLoc = 0;
